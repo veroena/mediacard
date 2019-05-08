be828bb2ae262062d172178921d5cde0158355f8
veroena <veronica.enamorado@gmail.com>
2019-05-08 08:37:41 +0200

    Add docs

---
+++ docs/build/precache-manifest.10588bba85aef3d26ba1be20acb5728b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8ad102ff6168d65c41ec21113d2893",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c24a4c6c7210bf4e9119",
+    "url": "./static/css/main.ac617c1e.chunk.css"
+  },
+  {
+    "revision": "94af7f8b07d7d4877212",
+    "url": "./static/js/2.15fbe00d.chunk.js"
+  },
+  {
+    "revision": "c24a4c6c7210bf4e9119",
+    "url": "./static/js/main.3d2a45e3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
